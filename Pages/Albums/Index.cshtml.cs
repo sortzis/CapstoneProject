a0e1d56e1eb29f85daaf64baa6e898fe9effb4e9
sortzis <sortzis@mail.nmc.edu>
2021-12-18 22:34:46 -0500

    sprint 2 commit

--- Pages/Albums/Index.cshtml.cs
+++ Pages/Albums/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CapstoneProject.Data;
 using CapstoneProject.Models;
@@ -20,10 +21,41 @@ namespace CapstoneProject.Pages.Albums
         }
 
         public IList<Album> Album { get;set; }
-
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public string ArtistSort { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string AlbumGenres { get; set; }
+        public SelectList Genre { get; set; }
+        public async Task OnGetAsync(string sortOrder)
         {
-            Album = await _context.Album.ToListAsync();
+            ArtistSort = sortOrder;
+
+            IQueryable<string> genreQuery = from g in _context.Album
+                                            orderby g.Genre
+                                            select g.Genre;
+
+            var albums = from m in _context.Album 
+                         select m;
+
+            if(!string.IsNullOrEmpty(SearchString))
+            {
+                albums = albums.Where(m => m.Artist.Contains(SearchString));
+            }
+
+            if (ArtistSort == "desc")
+            {
+                albums = albums.OrderByDescending(x => x.Artist);
+            }
+            
+            else
+            {
+                albums = albums.OrderBy(x => x.Artist);
+            }
+
+            Album = await albums.AsNoTracking().ToListAsync();
+
+            Genre = new SelectList(await genreQuery.Distinct().ToListAsync());
         }
     }
 }
